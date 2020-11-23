c1ac9604ac5296a99792b37492a77ef87f1eef2b
Elena Zarubina <ezarubina@croc.ru>
2020-11-23 14:19:19 +0300

    1.7. Состояние объекта [#404781]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -8,7 +8,7 @@ public class Battery {
     }
 
     public void exchange(Battery another) {
-        this.load = this.load + another.load;
-        another.load = 0;
+        another.load = another.load + this.load;
+        this.load = 0;
     }
 }
