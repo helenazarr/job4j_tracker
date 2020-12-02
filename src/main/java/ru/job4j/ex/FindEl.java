b715250ba58ac36855a28dcd1fd55148a96b1330
Elena Zarubina <ezarubina@croc.ru>
2020-12-02 20:07:33 +0300

    0.4. Пользовательские исключения. [#404746]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,27 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException{
+        int rsl = -1;
+        int i = 0;
+        for (String el : value) {
+            if (el == key) {
+                rsl = i;
+            }
+            i++;
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Element is not found");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            String[] value = {"1","2","3","4"};
+            System.out.println(indexOf(value, "5"));
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
